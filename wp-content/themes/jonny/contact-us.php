777bbb969f4c5d5696a2ca9031b7d6929b18090b
leduyhoang1994 <leduyhoang1994@gmail.com>
2021-01-06 12:19:57 +0700

    add cta

--- wp-content/themes/jonny/contact-us.php
+++ wp-content/themes/jonny/contact-us.php
@@ -128,15 +128,15 @@ if (isset($fullTitle[1])) {
             <div class="text-center">
                 <h1>Where to find us</h1>
             </div>
-            <div class="container-fluid">
-                <div style="display: flex">
-                    <div style="width: 40%; display: inline-block">
+            <div class="container-fluid text-center">
+                <div style="display: inline-flex; width: 80%">
+                    <div style="width: 40%; display: inline-block;" class="text-left">
                         <div class="text-center">
                             <h4>INFORMATION</h4>
                         </div>
                         <div>
                             <div>
-                                <h5>Orient Success International Investment JSC;</h5>
+                                <h5>Orient Success International Investment JSC.,</h5>
                             </div>
                             <div>
                                 3rd Floor, N01-T4 Building Diplomatic Corps Urban Area, <br>
@@ -162,8 +162,8 @@ if (isset($fullTitle[1])) {
                         <div class="text-center">
                             <h4>MAP</h4>
                         </div>
-                        <div>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7446.480371937826!2d105.7934246!3d21.0630671!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0b737fdff3%3A0xdcab1384832f79e6!2zUGjDuiBN4bu5IENvbXBsZXggTjAxIFQ0!5e0!3m2!1sen!2s!4v1609910084143!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <div style="display: flex">
+                            <iframe style="flex: 1" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7446.480371937826!2d105.7934246!3d21.0630671!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0b737fdff3%3A0xdcab1384832f79e6!2zUGjDuiBN4bu5IENvbXBsZXggTjAxIFQ0!5e0!3m2!1sen!2s!4v1609910084143!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                         </div>
                     </div>
                 </div>
